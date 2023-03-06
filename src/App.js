634430bc52278a08107024cfaae7a16304745905
Siddhant51 <124143317+Siddhant51@users.noreply.github.com>
2023-03-06 23:44:19 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,8 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
 
 function App() {
-  const [user, setLoginUser] = useState();
+  const userData = JSON.parse(localStorage.getItem("userData"));
+  const [user, setLoginUser] = useState(userData || null);
   return (
     <div className="App">
       <Router>
